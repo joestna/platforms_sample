640db548850a8335785baf0df19e35be78974e68
Jordi <jonana01@floridauniversitaria.es>
2022-01-31 23:00:50 +0100

    MV P3 E5 El personaje se mueve y se invierte el sprite segun el lado

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float velocidad = 1f;
+    float fuerza = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,46 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetButton("Horizontal"))
+        {
+            Rigidbody2D movPlayer = GetComponent<Rigidbody2D>();
+            float movement = Input.GetAxis("Horizontal");
+            
+
+            //transform.position += velocity * Vector3.right * 0.0001f * Time.deltaTime;
+
+            if (movement > 0)
+            {
+                if(transform.localScale.x < 0)
+                {
+                    transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+                }               
+
+                movPlayer.velocity = (transform.right * velocidad * movement);
+            }
+            else
+            {
+                if (transform.localScale.x > 0)
+                {
+                    transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
+                }
+
+                movPlayer.velocity = (transform.right * velocidad * movement);
+                //movPlayer.transform.localScale = new Vector3(0f, movPlayer.transform.localScale.y * -1f, 0f);
+            }
+
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            //float movement = Input.GetAxis("Jump");
+
+            Rigidbody2D movPlayer = GetComponent<Rigidbody2D>();
+            movPlayer.AddForce(transform.up * fuerza);
+
+            //transform.position += velocity * Vector3.right * 0.0001f * Time.deltaTime;
+        }
     }
 }
